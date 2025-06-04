366a74cfc65770fe8053056691840d76bdaa272d
Julio <julioplx2@gmail.com>
2025-06-04 13:35:41 -0600

    ✨ Eliminar la función create_map y reorganizar importaciones en main.py para mejorar la estructura del código

--- app/main.py
+++ app/main.py
@@ -1,15 +1,6 @@
-from fastapi import FastAPI, HTTPException
-from app.services.map import generate_map
-from app.schemas.markers import MarkerList
-
+from fastapi import FastAPI
+from app.api import routes
 
 app = FastAPI(title="Map Generation Microservice")
 
-
-@app.post("/generate-map/")
-def create_map(marker_list: MarkerList):
-    try:
-        path = generate_map(marker_list.markers)
-        return {"map_file_path": path}
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+app.include_router(routes.router, prefix="/api")
